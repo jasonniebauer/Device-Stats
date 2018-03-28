5d873a71353b7eae0ab2d1eb5badc3784de23294
Jason Niebauer <lookwhatjasondid@gmail.com>
2018-03-28 10:07:30 -0500

    Create geolocation-stats.js

---
+++ scripts/geolocation-stats.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Learn more
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+
+
+//
+if (navigator.geolocation) {
+  /* geolocation is available */
+  console.log('geolocation is available');
+  
+  var geo = navigator.geolocation;
+  
+  // console.log(geo);
+  
+  // navigator.geolocation.getCurrentPosition(function(position) {
+  //   // do_something(position.coords.latitude, position.coords.longitude);
+  //   console.log('Lat:' + position.coords.latitude + ', Long: ' + position.coords.longitude);
+  // });
+  
+  
+} else {
+  /* geolocation IS NOT available */
+  console.log('geolocation IS NOT available');
+}
