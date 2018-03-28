f25c635b262433d75c9f41d150b27a74933140b7
Jason Niebauer <lookwhatjasondid@gmail.com>
2018-03-28 10:10:01 -0500

    Create storage-stats.js

---
+++ scripts/storage-stats.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// window.localStorage
+// stores data with no expiration date
+//
+// window.sessionStorage
+// stores data for one session (data is lost when the browser tab is closed)
+
+if (typeof(Storage) !== "undefined") {
+    // Code for localStorage/sessionStorage.
+} else {
+    // Sorry! No Web Storage support..
+}
