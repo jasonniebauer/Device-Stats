a7d7175329198651bcb161b57960a2d20d3fdf97
Jason Niebauer <lookwhatjasondid@gmail.com>
2018-03-28 21:22:53 -0500

    Update connection-stats.js

--- scripts/connection-stats.js
+++ scripts/connection-stats.js
@@ -1,20 +1,82 @@
 'use strict';
 
-// NetworkInformation {
-//   downlink: 10,
-//   downlinkMax: Infinity,
-//   effectiveType: "4g",
-//   onchange: null,
-//   ontypechange: null,
-//   rtt: 100,
-//   type: "wifi"
-// }
+/***********************************
+* Network Information API
+* https://wicg.github.io/netinfo
+************************************
+* returns:
+*
+* NetworkInformation {
+*   downlink: 10,
+*   downlinkMax: Infinity,
+*   effectiveType: "4g",
+*   onchange: null,
+*   ontypechange: null,
+*   rtt: 100,
+*   type: "wifi"
+* }
+*
+***********************************/
 
-// support for network connection
+// Check support for network connection
 if (navigator.connection) {
-  var connectionInfo = navigator.connection;
+  // network connection detection is supported
+  var connection = navigator.connection;
+  
+  switch (conntection.effectiveType) {
+    case '2g':
+      // Do something
+      break;
+    case '3g':
+      // Do something
+      break;
+    case '4g':
+      // Do something
+      break;
+    case 'slow-2g':
+      // Do something
+      break;
+  }
+  
+  switch (connection.type) {
+    case 'bluetooth':
+      // Do something
+      break;
+    case 'cellular':
+      // Do something
+      break;
+    case 'ethernet':
+      // Do something
+      break;
+    case 'mixed':
+      // Do something
+      break;
+    case 'none':
+      // Do something
+      break;
+    case 'other':
+      // Do something
+      break;
+    case 'unknown':
+      // Do something
+      break;
+    case 'wifi':
+      // Do something
+      break;
+    case 'wimax':
+      // Do something
+      break;
+  }
+  
+  function updateConnectionStatus() {
+    // console.log("Connection type changed from " + type + " to " + connection.type);
+  }
 
-  console.log(connectionInfo);
+connection.addEventListener('typechange', updateConnectionStatus);
+
+connection.addEventListener('change', updateConnectionStatus);
+
+  console.log(connection);
 } else {
   
 }
